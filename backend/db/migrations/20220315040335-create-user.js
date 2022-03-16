968940b7159657cd6f17c81f744fd2fdcdbfbdf9
gisales92 <isales.greg@gmail.com>
2022-03-15 20:24:00 -0700

    Added instance and static methods to users models & added utils/auth.js

---
+++ backend/db/migrations/20220315040335-create-user.js
@@ -0,0 +1,58 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        unique: true,
+      },
+      firstName: {
+        type: Sequelize.STRING(20),
+      },
+      lastName: {
+        type: Sequelize.STRING(20),
+      },
+      previewImage: {
+        type: Sequelize.STRING,
+      },
+      bio: {
+        type: Sequelize.STRING(512),
+      },
+      city: {
+        type: Sequelize.STRING(25),
+      },
+      country: {
+        type: Sequelize.STRING(40),
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
