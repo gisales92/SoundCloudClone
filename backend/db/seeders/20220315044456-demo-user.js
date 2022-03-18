c5a51d8fe86cf7558113ffa8a1bb3d1ddbc5f367
gisales92 <isales.greg@gmail.com>
2022-03-17 21:09:02 -0700

    Added seed data to DB

--- backend/db/seeders/20220315044456-demo-user.js
+++ backend/db/seeders/20220315044456-demo-user.js
@@ -9,16 +9,22 @@ module.exports = {
         {
           email: 'demo@user.io',
           username: 'Demo-lition',
+          firstName: "First",
+          lastName: "Demo-user",
           hashedPassword: bcrypt.hashSync('password'),
         },
         {
           email: 'user1@user.io',
           username: 'FakeUser1',
+          firstName: "Segundo",
+          lastName: "Fakeuser",
           hashedPassword: bcrypt.hashSync('password2'),
         },
         {
           email: 'user2@user.io',
           username: 'FakeUser2',
+          firstName: "Tercer",
+          lastName: "Fakeuser",
           hashedPassword: bcrypt.hashSync('password3'),
         },
       ],
