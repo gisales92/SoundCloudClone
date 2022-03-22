32da40127ddb9688c01d35f2208a6bff1ad9478e
gisales92 <isales.greg@gmail.com>
2022-03-21 22:09:00 -0700

    Added the edit/delete songs route

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -8,7 +8,10 @@ const {
   requireAuth,
 } = require("../../utils/auth");
 const { Song, User, Album } = require("../../db/models");
-const { handleValidationErrors } = require("../../utils/validation");
+const {
+  handleValidationErrors,
+  validateSong,
+} = require("../../utils/validation");
 const router = express.Router();
 
 router.get(
@@ -60,11 +63,72 @@ router.get(
       };
       res.json(mappedSong);
     } else {
-        const err = new Error("Song couldn't be found");
-        err.status = 404;
-        next(err)
+      const err = new Error("Song couldn't be found");
+      err.status = 404;
+      next(err);
     }
   })
 );
+
+router.put(
+  "/:songId",
+  requireAuth,
+  validateSong,
+  asyncHandler(async (req, res, next) => {
+    const songId = req.params.songId;
+    const userId = req.user.id;
+    const song = await Song.findByPk(songId);
+    if (song) {
+      if (song.userId === userId) {
+        const { title, description, url, imageUrl } = req.body;
+        await song.update({
+          title,
+          description,
+          soundFileURL: url,
+          previewImage: imageUrl,
+        });
+        const updatedSong = {
+          id: song.id,
+          userId: song.userId,
+          albumId: song.albumId,
+          title: song.title,
+          description: song.description,
+          url: song.soundFileURL,
+          createdAt: song.createdAt,
+          updatedAt: song.updatedAt,
+          previewImage: song.previewImage,
+        };
+        return res.json(updatedSong);
+      } else {
+        res.status(403);
+        res.json({ message: "Forbidden", statusCode: 403 });
+      }
+    } else {
+      res.status(404);
+      return res.json({ message: "Song couldn't be found", statusCode: 404 });
+    }
+  })
+);
+
+router.delete("/:songId",
+requireAuth,
+asyncHandler(async (req, res, next) => {
+  const songId = req.params.songId;
+    const userId = req.user.id;
+    const song = await Song.findByPk(songId);
+    if (song) {
+      if (song.userId === userId) {
+         await song.destroy();
+         res.status(200);
+         return res.json({message: "Successfully deleted", statusCode: 200})
+      } else {
+        res.status(403);
+        res.json({ message: "Forbidden", statusCode: 403 });
+      }
+    } else {
+      res.status(404);
+      return res.json({ message: "Song couldn't be found", statusCode: 404 });
+    }
+}))
 
 module.exports = router;
