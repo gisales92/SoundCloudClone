0ae38f7f30ce50076884d371d497feb2f7f914e3
gisales92 <isales.greg@gmail.com>
2022-05-04 20:23:48 -0700

    Altered restoreUser thunk to not error out when there was no user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Switch, Route } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormModal";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import { userSelector } from "./store/session";
 import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+  const user = useSelector(userSelector);
+
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
@@ -16,7 +19,7 @@ function App() {
   return (
     isLoaded && (
       <>
-        <Navigation isLoaded={isLoaded}/>
+        <Navigation isLoaded={isLoaded} />
         <Switch>
           <Route path="/login">
             <LoginFormPage />
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,38 +33,40 @@ export const login = (user) => async (dispatch) => {
 };
 
 export const restoreUser = () => async (dispatch) => {
+  try {
     const response = await csrfFetch("/api/session");
-    const data = await response.json();
-    dispatch(setUser(data.user));
-    return response;
-  };
-
-  export const signup = (user) => async (dispatch) => {
-    const { username, email, password, firstName, lastName } = user;
-    const response = await csrfFetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username,
-        email,
-        firstName,
-        lastName,
-        password,
-      }),
-    });
     const data = await response.json();
     dispatch(setUser(data));
     return response;
-  };
+  } catch {}
+};
 
-  export const logout = () => async (dispatch) => {
-      const response = await csrfFetch('/api/session', {
-        method: 'DELETE',
-      });
-      dispatch(removeUser());
-      return response;
-    };
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password, firstName, lastName } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      firstName,
+      lastName,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
 
-const sessionReducer = (state = {user: null}, action) => {
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "DELETE",
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+const sessionReducer = (state = { user: null }, action) => {
   let newState;
   switch (action.type) {
     case SET_USER:
