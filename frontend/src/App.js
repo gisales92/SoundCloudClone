4532ad4c66a9c08d81a3494115021fb2d86e2c8a
gisales92 <isales.greg@gmail.com>
2022-05-04 21:15:19 -0700

    Adjusted CSS for Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Switch, Route } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormModal";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
-import { userSelector } from "./store/session";
 import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-  const user = useSelector(userSelector);
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
