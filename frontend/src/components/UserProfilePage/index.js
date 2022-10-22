62fac6140bbbdf94ff3accf9074172b885d3722e
gisales92 <isales.greg@gmail.com>
2022-10-21 22:20:05 -0700

    adding additional profile info to profile header

--- frontend/src/components/UserProfilePage/index.js
+++ frontend/src/components/UserProfilePage/index.js
@@ -8,8 +8,7 @@ import {
   useLocation,
 } from "react-router-dom";
 import { userSelector } from "../../store/session";
-import "./profile.css"
-
+import "./profile.css";
 
 const UserProfilePage = (props) => {
   const dispatch = useDispatch();
@@ -35,26 +34,36 @@ const UserProfilePage = (props) => {
   return (
     <div className="profile-page">
       <div className="profile-header">
-        <img src={user.previewImage} alt="profile" className="user-profile-pic"/>
-        <h2 className="profile user-name">{user.username}</h2>
+        <img
+          src={user.previewImage}
+          alt="profile"
+          className="user-profile-pic"
+        />
+        <div className="profile header-info">
+          <h2 className="profile user-name">{user.username}</h2>
+          <p className="profile header-sub">{`${user.firstName} ${user.lastName}`}</p>
+          <p className="profile header-sub">{`${user.city}, ${user.country}`}</p>
+        </div>
       </div>
       <ul className="profile-navigation">
         <li key="favorites">
           <NavLink
             to="/profile/tracks"
             className="main-color-hover"
-            isActive={() =>
-              ["/profile", "/profile/tracks"].includes(pathname)
-            }
+            isActive={() => ["/profile", "/profile/tracks"].includes(pathname)}
           >
             Tracks
           </NavLink>
         </li>
         <li key="playlists">
-          <NavLink to="/profile/playlists" className="main-color-hover">Playlists</NavLink>
+          <NavLink to="/profile/playlists" className="main-color-hover">
+            Playlists
+          </NavLink>
         </li>
         <li key="reviews">
-          <NavLink to="/profile/comments" className="main-color-hover">Comments</NavLink>
+          <NavLink to="/profile/comments" className="main-color-hover">
+            Comments
+          </NavLink>
         </li>
       </ul>
       <Switch>
