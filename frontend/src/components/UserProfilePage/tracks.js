0790eb5abf81f8bcdd06b9f8fa248e199969c71e
gisales92 <isales.greg@gmail.com>
2022-10-24 15:04:09 -0700

    added alt for no tracks on profile

--- frontend/src/components/UserProfilePage/tracks.js
+++ frontend/src/components/UserProfilePage/tracks.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { songsByArtistId } from "../../store/song";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
 import SongListThumb from "../SongListThumb";
 
 
@@ -13,13 +13,16 @@ function UserTracks(userId) {
   return (
     <div className="user-tracks-outer">
 
-      <ul className="index-songs-list">
+     {songs.length ? <ul className="index-songs-list">
         {Object.keys(songs)?.map((songId) => {
           if (!isNaN(parseInt(songId))) {
             return <SongListThumb key={songId} song={songs[songId]} />;
           }
         })}
-      </ul>
+      </ul> : <div className="no-data-outer">
+      <FontAwesomeIcon className="no-data icon" icon={faMusic} />
+      <p className="profile no-data">Seems a little quiet over here</p>
+        </div>}
     </div>
   );
 }
