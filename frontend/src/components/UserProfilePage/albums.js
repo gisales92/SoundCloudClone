4849a096da6da2a602c5c508303f12fe8e2f446d
gisales92 <isales.greg@gmail.com>
2022-10-26 19:33:54 -0700

    added album cover art to profile

--- frontend/src/components/UserProfilePage/albums.js
+++ frontend/src/components/UserProfilePage/albums.js
@@ -1,8 +1,8 @@
 import { useSelector } from "react-redux";
 import { albumsByArtistsSelector } from "../../store/albums";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
-import {timestamper} from "../SongDetail"
+import { faMusic, faRecordVinyl } from "@fortawesome/free-solid-svg-icons";
+import { timestamper } from "../SongDetail";
 
 function UserAlbums() {
   const albums = useSelector(albumsByArtistsSelector);
@@ -13,17 +13,29 @@ function UserAlbums() {
         {Object.keys(albums).length ? (
           <ul className="index-albums-list">
             {Object.keys(albums)?.map((albumId) => {
+              console.log();
               return (
-                <div className="user-album-outer">
-                    <p className="album-title profile">{albums[albumId].title}</p>
-                    <p className="album-time profile">{`${timestamper(albums[albumId].createdAt)} ago`}</p>
+                <div key={albumId} className="user-album-outer">
+                  {albums[albumId].previewImage ? (
+                    <img
+                      src={albums[albumId].previewImage}
+                      crossOrigin=""
+                      alt="album cover"
+                      className="album-cover"
+                    />
+                  ) : (
+                    <FontAwesomeIcon className="album-cover icon" icon={faRecordVinyl} />
+                  )}
+                  <p className="album-title profile">{albums[albumId].title}</p>
+                  <p className="album-time profile">{`${timestamper(
+                    albums[albumId].createdAt
+                  )} ago`}</p>
                 </div>
               );
             })}
           </ul>
         ) : (
           <div className="no-data-outer">
-            <FontAwesomeIcon className="no-data icon" icon={faMusic} />
             <p className="profile no-data">Seems a little quiet over here</p>
           </div>
         )}
